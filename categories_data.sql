8cb08aed796a6616b173f4afdfe2c2e8a7a26722
Blacking84 <alexey@lugachev.ru>
2022-08-24 00:40:05 +0700

    Повелитель данных

---
+++ categories_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO categories (`name`, `icon`) VALUE ("Курьерские услуги", "courier");
+INSERT INTO categories (`name`, `icon`) VALUE ("Уборка", "clean");
+INSERT INTO categories (`name`, `icon`) VALUE ("Переезды", "cargo");
+INSERT INTO categories (`name`, `icon`) VALUE ("Компьютерная помощь", "neo");
+INSERT INTO categories (`name`, `icon`) VALUE ("Ремонт квартирный", "flat");
+INSERT INTO categories (`name`, `icon`) VALUE ("Ремонт техники", "repair");
+INSERT INTO categories (`name`, `icon`) VALUE ("Красота", "beauty");
+INSERT INTO categories (`name`, `icon`) VALUE ("Фото", "photo");
---
+++ src/exception/SourceFileException.php
@@ -0,0 +1,9 @@
+<?php
+namespace Blacking\TaskForce\exception;
+
+use Exception;
+
+class SourceFileException extends Exception
+{
+
+}
